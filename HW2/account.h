ff5c3bfd61bd307f641129e1bf394e70b1726281
ydreizin <yuvaldreizin@campus.technion.ac.il>
2025-06-01 14:15:21 +0300

    Refactor account and ATM handling to use string passwords, improving security and memory management

--- HW2/account.h
+++ HW2/account.h
@@ -10,7 +10,7 @@
 * Structures
 * =============================================================================*/
 
-account_with_id *account_init(int id, int pass, int balance);
+account_with_id *account_init(int id, char* pass, int balance);
 void account_free(void *acc);
 // int account_get_id(account *account);
 // int account_get_pass(account *account);
@@ -22,16 +22,16 @@ void account_write_unlock(account *account);
 
 int accounts_compare(void *acnt1, void *acnt2);
 account *account_check_id_read(int id);
-account *account_check_id_and_pass_read(int id, int pass, int atm_id);
+account *account_check_id_and_pass_read(int id, char* pass, int atm_id);
 account *account_check_id_write(int id);
-account *account_check_id_and_pass_write(int id, int pass, int atm_id);
+account *account_check_id_and_pass_write(int id, char* pass, int atm_id);
 
-f_status_t account_o(int id, int pass, int initial_amount, int atm_id);
-f_status_t account_d(int id, int pass, int amount, int atm_id);
-f_status_t account_w(int id, int pass, int amount, int atm_id);
-f_status_t account_b(int id, int pass, int atm_id);
-f_status_t account_q(int id, int pass, int atm_id);
-f_status_t account_t(int id, int pass, int amount, int to_id, int atm_id);
+f_status_t account_o(int id, char* pass, int initial_amount, int atm_id);
+f_status_t account_d(int id, char* pass, int amount, int atm_id);
+f_status_t account_w(int id, char* pass, int amount, int atm_id);
+f_status_t account_b(int id, char* pass, int atm_id);
+f_status_t account_q(int id, char* pass, int atm_id);
+f_status_t account_t(int id, char* pass, int amount, int to_id, int atm_id);
 f_status_t account_print(account *acnt);
 void lock_all_accounts();
 void unlock_all_accounts();
